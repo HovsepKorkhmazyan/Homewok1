b280f4353b3ee79610de31c6eba8485bb1516d04
HovsepKorkhmazyan <hovsepwarriors@gmail.com>
2023-09-12 12:56:48 +0300

    Homework1 FigurePainter

---
+++ src/Sort.java
@@ -0,0 +1,30 @@
+public class Sort {
+    public static void main(String[] args) {
+        int[] array = {4,7,1,3,9,0,2};
+        for (int x :
+                array) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+
+        int temp;
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i; j < array.length; j++) {
+                if (array[i] > array[j]){
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+
+            }
+
+        }
+        System.out.println();
+        for (int x: array) {
+            System.out.print(x + " ");
+        }
+
+        }
+    }
+
