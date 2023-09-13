30232f7602ce7bbe7980a75a590acda471e142f4
HovsepKorkhmazyan <hovsepwarriors@gmail.com>
2023-09-13 12:48:00 +0300

    Homework1 FigurePainter

--- src/Sort.java
+++ src/Sort.java
@@ -1,8 +1,7 @@
 public class Sort {
     public static void main(String[] args) {
         int[] array = {4,7,1,3,9,0,2};
-        for (int x :
-                array) {
+        for (int x : array) {
             System.out.print(x + " ");
         }
         System.out.println();
